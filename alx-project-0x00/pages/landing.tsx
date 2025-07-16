fdd63a84e740bc3164a2bab72aef3e3c42abd2a9
Lapidis2 <hitlapidis2@gmail.com>
2025-07-16 16:29:59 +0200

    edited landing page

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -10,6 +10,7 @@ const Landing: React.FC =  () => {
       <div className="flex flex-row gap-3">
         <Button title="Small Rounded" styles="text-sm rounded-sm" />
         <Button title="Medium Rounded" styles="text-base rounded-md" />
+		<Button title="Large Rounded" styles="text-lg rounded-lg" />
         <Button title="Large Rounded Full" styles="text-lg rounded-full" />
         
       </div>
